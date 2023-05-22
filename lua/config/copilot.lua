9ed4aec79d0859cad1da5e755e05a089eab354cb
Mafrans <malte.kluft@gmail.com>
2023-05-22 09:17:12 +0200

    feat: add (unused) copilot helper

---
+++ lua/config/copilot.lua
@@ -0,0 +1,12 @@
+return function()
+  local copilot = require "copilot"
+
+  copilot.setup({
+      suggestion = {
+          enabled = false,
+      },
+      panel = {
+          enabled = false,
+      },
+  })
+end
